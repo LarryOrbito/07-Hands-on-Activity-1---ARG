01f1a57b7c93491bf104d9f1a33be9c1e5ed9a81
LarryOrbito <larryorbito@gmail.com>
2024-12-03 22:25:48 +0800

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,16 @@
+class Dog extends Animal {
+    
+    public void eat() {
+        System.out.println("Dogs love to eat bones.");
+    }
+
+   
+    public void sleep() {
+        System.out.println("Dogs sleep for 7-9 hours a day.");
+    }
+
+   
+    public void makeSound() {
+        System.out.println("Woof, woof");
+    }
+}
