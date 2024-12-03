c123ea72dcc291e7dcec41b7978c542eb868d9d4
LarryOrbito <larryorbito@gmail.com>
2024-12-03 22:27:31 +0800

    Create Bird.java

---
+++ Birdd.java
@@ -0,0 +1,16 @@
+class Bird extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("Birds love to eat seeds.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Birds sleep for 10-12 hours a day.");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Tweet, tweet");
+    }
+}
