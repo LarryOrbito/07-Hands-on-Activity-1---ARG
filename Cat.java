fb5046500c41803c130fdf6779cd25e9f7b86def
LarryOrbito <larryorbito@gmail.com>
2024-12-03 22:26:37 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,12 @@
+class Cat extends Animal {
+    
+    public void eat() {
+        System.out.println("Cats love to eat fish.");
+    }
+    public void sleep() {
+        System.out.println("Cats sleep for 3-6 hours a day.");
+    }  
+    public void makeSound() {
+        System.out.println("Meow, meow");
+    }
+}
